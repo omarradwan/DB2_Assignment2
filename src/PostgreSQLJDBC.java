41c7ddd79bf1755e6e940ffe595d8b54d642281a
Omar Radwan <ommarradwan213@gmail.com>
2016-04-11 00:14:54 +0200

    inserting records

--- src/PostgreSQLJDBC.java
+++ src/PostgreSQLJDBC.java
@@ -1,15 +1,14 @@
 import java.sql.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class PostgreSQLJDBC {
-   public static void main( String args[] )
+   public static void main( String args[] ) throws SQLException
      {
        Connection c = null;
        Statement stmt = null;
+       ResultSet rs = null;
        try {
          Class.forName("org.postgresql.Driver");
          c = DriverManager
@@ -18,7 +17,7 @@ public class PostgreSQLJDBC {
          System.out.println("Opened database successfully");
 
          stmt = c.createStatement();
-         String sql1 = "CREATE TABLE cup_matches" +
+         /*String sql1 = "CREATE TABLE cup_matches" +
                       "(mid INT PRIMARY KEY     NOT NULL," +
                       " round           varchar(50)    NOT NULL, " +
                       " year            INT     NOT NULL, " +
@@ -31,16 +30,37 @@ public class PostgreSQLJDBC {
                  " year            INT     NOT NULL, " +
                  " position        INT, " +
                  " primary key(mid , name))";
+         */
          
+         for (int i = 0; i < 58960; i++) {
+        	 String sql1 = "";
+        	 if(i < 118)
+        		 sql1 = "insert into player_in values("+i%2680+", 'pele' , 1950 , 10)";
+        	 
+        	 else if(i < 20000)
+        		 sql1 = "insert into player_in values("+i%2680+", 'Ronaldo"+i+"' , 2008 , 7)";
+        	 
+        	 else if(i < 40000)
+        		 sql1 = "insert into player_in values("+i%2680+", 'Abo-Treka"+i+"' , 2005 , 22)";
+        	 
+        	 else
+        		 sql1 = "insert into player_in values("+i%2680+", 'Puyol"+i+"' , 2000 , 5)";
 
-         stmt.executeUpdate(sql1);
-         stmt.executeUpdate(sql2);
+        		 
+        	 stmt.executeUpdate(sql1);
+		}
+         System.out.println("Records inserted successfully");
+         String sql2 = "select count (*) from player_in";
+         rs =  stmt.executeQuery(sql2);
+         if(rs.next())
+        	 System.out.println(rs.getInt("count"));
+         rs.close();
          stmt.close();
          c.close();
        } catch ( Exception e ) {
          System.err.println( e.getClass().getName()+": "+ e.getMessage() );
          System.exit(0);
        }
-       System.out.println("Table created successfully");
+     //  System.out.println("Records inserted successfully");
      }
 }
